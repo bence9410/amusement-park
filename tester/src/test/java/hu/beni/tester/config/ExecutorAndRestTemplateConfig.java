392f4899cdd976e23effda0c5e629e3540170b27
Bence <nembence1994@gmail.com>
2020-05-03 21:32:02 +0200

    Remove leftover RestTemplate interceptor.

--- tester/src/test/java/hu/beni/tester/config/ExecutorAndRestTemplateConfig.java
+++ tester/src/test/java/hu/beni/tester/config/ExecutorAndRestTemplateConfig.java
@@ -11,23 +11,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
-import org.springframework.http.HttpRequest;
-import org.springframework.http.client.ClientHttpRequestExecution;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
-import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.util.FileCopyUtils;
 import org.springframework.web.client.RestTemplate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
 @EnableAsync
 @Configuration
 @RequiredArgsConstructor
@@ -54,18 +47,7 @@ public class ExecutorAndRestTemplateConfig {
 	@Scope(SCOPE_PROTOTYPE)
 	public RestTemplate restTemplate() {
 		RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
-		// restTemplate.getInterceptors().add(createLoggingInterceptor());
 		return restTemplate;
 	}
 
-	private ClientHttpRequestInterceptor createLoggingInterceptor() {
-		return (HttpRequest request, byte[] body, ClientHttpRequestExecution execution) -> {
-			log.info("Request: { URL: {}, Method: {}, Body: {} }", request.getURI(), request.getMethod(),
-					new String(body));
-			ClientHttpResponse response = execution.execute(request, body);
-			log.info("Response: { Status: {}, Body: {} }", response.getStatusCode().toString(),
-					new String(FileCopyUtils.copyToByteArray(response.getBody())));
-			return response;
-		};
-	}
 }
